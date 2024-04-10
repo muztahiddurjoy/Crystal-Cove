f3077ce1ef95e0cd6dbc647deacb466094c04900
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-04-11 01:35:29 +0600

    navbar designed

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,15 @@
+import { Button, Flex, Spacer, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Flex p={3} bg="orange.400" alignItems="center">
+        <Text color="white" fontWeight={700}>Crystal Cove</Text>
+        <Spacer/>
+        <Link to="/join">
+            <Button size="sm" _hover={{bg:"orange.100", color:"orange.500"}} color="white" variant="outline">Login</Button>
+        </Link>
+    </Flex>
   )
 }
 
