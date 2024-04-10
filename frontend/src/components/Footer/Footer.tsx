8f6a27c16eda3717f9f472c56adf9bca2f58c34c
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-04-11 01:24:53 +0600

    footer designed

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
+import { Box, Center, Flex, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box>
+        <Flex wrap={"wrap"} alignItems="center" justifyContent="center" bg="orange.400" p={3} gap={5}>
+            <Link to="/room/coral-chamber">
+                <Text fontWeight={500} fontSize={14} color="white">Coral Chamber</Text>
+            </Link>
+            <Link to="/room/seashell-suite">
+                <Text fontWeight={500} fontSize={14} color="white">Seashell Suite</Text>
+            </Link>
+            <Link to="/room/harbor-haven">
+                <Text fontWeight={500} fontSize={14} color="white">Harbor Haven</Text>
+            </Link>
+        </Flex>
+        <Center bg="orange.500" p={2}>
+            <Text fontSize={12} color="white">Copyright © {new Date().getFullYear()}. All rights reserved.</Text>
+        </Center>
+    </Box>
   )
 }
 
