b009369b60fe0a914330a773958b9bcf3f1d1d8a
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-04-12 01:01:30 +0600

    login form designed

--- frontend/src/components/Login/LoginForm/LoginForm.tsx
+++ frontend/src/components/Login/LoginForm/LoginForm.tsx
@@ -1,7 +1,16 @@
+import { Button, Card, Heading, Input, Text } from "@chakra-ui/react"
+import { useState } from "react"
 
 const LoginForm = () => {
+    const [email, setemail] = useState<string>('')
+    const [password, setpassword] = useState<string>('')
   return (
-    <div>LoginForm</div>
+    <Card mt={5} p={5}>
+        <Text fontSize="x-large" fontWeight={700} color="orange.500">Login</Text>
+        <Input _focus={{borderColor:'orange.500'}} value={email} onChange={(e)=>setemail(e.target.value)} placeholder="Email" my={5}/>
+        <Input _focus={{borderColor:'orange.500'}} value={password} onChange={(e)=>setpassword(e.target.value)} placeholder="Password" mb={5}/>
+        <Button bg="orange.400" color="white" _hover={{bg: "orange.500"}} disabled={true}>Login</Button>
+    </Card>
   )
 }
 
