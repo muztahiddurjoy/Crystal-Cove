b36fe6ed339328f2d7f2667f5aa3f16e3041b787
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-04-12 01:06:32 +0600

    login page design almost done

--- frontend/src/components/Login/SignupForm/SignupForm.tsx
+++ frontend/src/components/Login/SignupForm/SignupForm.tsx
@@ -1,7 +1,19 @@
+import { Button, Card, Input, Text } from "@chakra-ui/react"
+import { useState } from "react"
 
 const SignupForm = () => {
+    const [email, setemail] = useState<string>('')
+    const [password, setpassword] = useState<string>('')
+    const [confirmpassword, setconfirmpassword] = useState<string>('')
+    
   return (
-    <div>SignupForm</div>
+    <Card my={5} p={5}>
+        <Text fontSize="x-large" fontWeight={700} color="orange.500">Sign-up</Text>
+        <Input _focus={{borderColor:'orange.500'}} value={email} onChange={(e)=>setemail(e.target.value)} placeholder="Email" my={5}/>
+        <Input _focus={{borderColor:'orange.500'}} value={password} onChange={(e)=>setpassword(e.target.value)} placeholder="Password" mb={5}/>
+        <Input _focus={{borderColor:'orange.500'}} value={confirmpassword} onChange={(e)=>setconfirmpassword(e.target.value)} placeholder="Confirm Password" mb={5}/>
+        <Button bg="orange.400" color="white" _hover={{bg: "orange.500"}} disabled={true}>Sign-up</Button>
+    </Card>
   )
 }
 
