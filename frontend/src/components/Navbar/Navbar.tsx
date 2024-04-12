0aa16369275f7a97480d6dfa06a605c92f19edda
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-04-12 12:42:42 +0600

    navbar home link added

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <Flex p={3} bg="orange.400" alignItems="center">
-        <Text color="white" fontWeight={700}>Crystal Cove</Text>
+      <Link to="/">
+        <Button variant="ghost" color="white" _hover={{bg:"rgba(255,255,255,0.2)"}}>Crystal Cove</Button>
+      </Link>
         <Spacer/>
         <Link to="/join">
             <Button size="sm" _hover={{bg:"orange.100", color:"orange.500"}} color="white" variant="outline">Login</Button>
